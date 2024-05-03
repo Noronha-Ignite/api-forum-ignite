f31c0e08a7381cd9bd749f77493fa15e0d515e7a
Gabriel Noronha <inc.691@gmail.com>
2024-05-03 00:17:19 -0300

    feat: include fetch question answers use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,4 +1,5 @@
 import { Answer } from '../../enterprise/entities/answer'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
@@ -6,4 +7,8 @@ export interface AnswersRepository {
   save(answer: Answer): Promise<void>
 
   findById(id: string): Promise<Answer | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<Answer[]>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -22,5 +23,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async findById(id: string): Promise<Answer | null> {
     return this.items.find((item) => item.id.toString() === id) ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    return this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
   }
 }
