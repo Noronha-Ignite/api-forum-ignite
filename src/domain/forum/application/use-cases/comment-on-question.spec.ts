175086a1914c6427e2fa3c9d068e307942354b60
Gabriel Noronha <inc.691@gmail.com>
2024-05-03 23:47:10 -0300

    feat: include comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,38 @@
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on question use case', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on a question', async () => {
+    const question = makeQuestion({}, new UniqueEntityID('question-1'))
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      authorId: 'author-1',
+      questionId: 'question-1',
+      content: 'This is a comment on the question',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items).toHaveLength(1)
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toBe(
+      'This is a comment on the question',
+    )
+  })
+})
---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,43 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error(`Could not find a question with Id: '${questionId}'`)
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      content,
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.questionCommentsRepository.create(questionComment)
+
+    return { questionComment }
+  }
+}
