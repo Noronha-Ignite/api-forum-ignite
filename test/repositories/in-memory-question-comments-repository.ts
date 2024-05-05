a674753a8ba208a518953f00f777038c75b16cf2
Gabriel Noronha <inc.691@gmail.com>
2024-05-05 18:50:13 -0300

    feat: include fetch question comments use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -18,5 +19,14 @@ export class InMemoryQuestionCommentsRepository
 
   async findById(id: string): Promise<QuestionComment | null> {
     return this.items.find((item) => item.id.toString() === id) ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    return this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
   }
 }
